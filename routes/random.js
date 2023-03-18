834cd8456e2a6a7253127f9b26d49f09a8336100
TheDogHusky <leredlabo@gmail.com>
2023-03-18 11:55:41 +0100

    optimized random

--- routes/random.js
+++ routes/random.js
@@ -2,11 +2,16 @@ const express = require('express');
 
 
 module.exports = {
-    path: "/random",
+    path: "/random/:type",
     method: "get",
     router: (req, res, data, logger) => {
+        const type = req.params.type;
+        if (!type) return res.status(400).json({ message: 'Bad request.' });
+        if (!['neko', 'kitsune', 'lewd', 'hug', 'kiss', 'pat'].includes(type)) return res.redirect('/random/neko');
+
         return res.render('random', {
-            page: req.url
+            page: req.url,
+            type: type
         });
     }
 };

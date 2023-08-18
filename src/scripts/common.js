ed2cac0d9f1192a67c846e6c68897fedde464769
Adam <classy.craft3r@gmail.com>
2023-08-18 23:46:28 +0200

    Clean code for merge

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,10 +1,10 @@
 function download(e) {
-  const element = e.target;
-  const url = element.getAttribute("url");
+    const element = e.target;
+    const url = element.getAttribute("url");
 
-  const elem = document.createElement("a");
-  elem.href = url;
-  elem.download = url.split("/").pop();
-  elem.click();
-  document.body.removeChild(elem);
+    const elem = document.createElement("a");
+    elem.href = url;
+    elem.download = url.split("/").pop();
+    elem.click();
+    document.body.removeChild(elem);
 }
--- src/scripts/random.js
+++ src/scripts/random.js
@@ -3,13 +3,13 @@ const apiURL = `${baseUrl}/api/v1/random/`;
 const type = window.location.pathname.split('/')[2];
 
 async function get() {
-  const data = await (await fetch(apiURL + type)).json();
-  const blob = await (await fetch(data.url)).blob();
-  const url = window.URL.createObjectURL(blob);
+    const data = await (await fetch(apiURL + type)).json();
+    const blob = await (await fetch(data.url)).blob();
+    const url = window.URL.createObjectURL(blob);
 
-  document.querySelector("#img").src = url;
-  document.querySelector("#download").setAttribute("url", url);
-  document.querySelector("#download").addEventListener("click", download);
+    document.querySelector("#img").src = url;
+    document.querySelector("#download").setAttribute("url", url);
+    document.querySelector("#download").addEventListener("click", download);
 }
 
 get();
@@ -29,7 +29,7 @@ function createI() {
     const i = document.createElement('i');
     i.classList.add('bi');
     i.classList.add('bi-arrow-counterclockwise');
-  return i;
+    return i;
 }
 
 /**
@@ -48,33 +48,33 @@ function changeLoadingState(element, state) {
         span.classList.add('spinner-border-sm');
         span.setAttribute('role', 'status');
         span.setAttribute('aria-hidden', true);
-  }
+    }
 
     if(state === true && !element.contains(span)) {
         const tempoSpan = element.querySelector('span#text');
         tempoSpan.textContent = ' New ' + type + ' pls';
-    return element.insertBefore(span, element.children[0]);
-  }
+        return element.insertBefore(span, element.children[0]);
+    }
     if(state === false && element.contains(span)) {
-    element.removeChild(span);
+        element.removeChild(span);
         const tempoSpan = element.querySelector('span#text');
-    const el = createI();
+        const el = createI();
         const text = document.createTextNode(' New ' + type + ' pls');
         tempoSpan.textContent = '';
-    tempoSpan.appendChild(el);
-    tempoSpan.appendChild(text);
-  }
+        tempoSpan.appendChild(el);
+        tempoSpan.appendChild(text);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-    let img = document.querySelector('#img')
+    let img = document.querySelector('#img');
     let btnNew = document.querySelector('#new');
 
     btnNew.addEventListener('click', async () => {
         changeClassState('disabled', btnNew, true);
-    changeLoadingState(btnNew, true);
+        changeLoadingState(btnNew, true);
         await get().catch(err => {});
         changeClassState('disabled', btnNew, false);
-    changeLoadingState(btnNew, false);
-  });
+        changeLoadingState(btnNew, false);
+    });
 });

398e4ff810c470707be836fbaf8949bc20a7dbfb
Adam <classy.craft3r@gmail.com>
2023-08-19 15:52:18 +0200

    Add support for slaps in API

--- routes/random.js
+++ routes/random.js
@@ -7,7 +7,7 @@ module.exports = {
     router: (req, res, data, logger) => {
         const type = req.params.type;
         if (!type) return res.status(400).json({ message: 'Bad request.' });
-        if (!['neko', 'kitsune', 'lewd', 'hug', 'kiss', 'pat'].includes(type)) return res.redirect('/random/neko');
+        if (!['neko', 'kitsune', 'lewd', 'hug', 'kiss', 'pat', 'slap'].includes(type)) return res.redirect('/random/neko');
 
         return res.render('random', {
             page: req.url,

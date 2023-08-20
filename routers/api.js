022b8a405c32af7ec4cc5df31171a611fca3ee5d
Adam <classy.craft3r@gmail.com>
2023-08-20 17:40:37 +0200

    Add 8ball in API
    /pi/v1/8ball(?cute=true)
    - Gives a 8ball answer (cute or not)

--- routers/api.js
+++ routers/api.js
@@ -81,5 +81,66 @@ api.get('/v1/random/:type', async (req, res) => {
     }
 });
 
+api.get('/v1/8ball', (req, res) => {
+    const cute = req.query.cute ? req.query.cute : false;
+
+    let answers = [
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes, definitely",
+        "You may rely on it",
+
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+
+        "Reply hazy try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"
+    ];
+
+    if(cute) {
+        answers = [
+            "Yes!!",
+            "Of Cwurse!",
+            "Yes, definitelwy nya!",
+            "Without any doubts nya!",
+            "My tail says yess!",
+
+            "Mhh.. I think so!",
+            "Most likely!",
+            "Outwook good!",
+            "Yesh!",
+            "Signs point to yes!",
+
+            "I don't know nya..",
+            "Ask again later, nya!",
+            "Better not twell you now!",
+            "Cannot predict now..",
+            "Concentrate and ask again! :3",
+
+            "Don't count on it! >:c",
+            ">-< My reply is nyo!",
+            "My sources say nyo!",
+            "Outlook nyot so good! :c",
+            "Very doubtfwul!"
+        ]
+    }
+
+
+    res.status(200).json({ answer: answers[Math.floor(Math.random() * answers.length)] });
+});
+
 
 module.exports = api;
